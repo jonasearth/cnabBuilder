4851acc379336ecdbbb4b54c0136a94087b3c3e4
jonas lima <jonasearth1@gmail.com>
2024-01-11 20:02:50 -0300

    feat: add seach by company and file location

--- README.md
+++ README.md
@@ -31,18 +31,16 @@ Opções:
   -t, --to        posição final de pesquisa da linha do Cnab
                                                           [número] [obrigatório]
   -s, --segmento  tipo de segmento                        [string] [obrigatório]
+  -d, --dir       caminho do arquivo                                    [string]
+  -c, --company   nome da empresa                                       [string]
 
 Exemplos:
-  cnabRows.js -f 21 -t 34 -s p  lista a linha e campo que from e to do cnab
+  cnabRows.js -f 21 -t 34 -s p              lista a linha e campo que from e to
+                                            do cnab
+  cnabRows.js -f 21 -t 34 -s p -c BRASIL    lista as empresas que contem BRASIL
+                                            no nome e salva no JSON
+  cnabRows.js -f 21 -t 34 -s p -d ./files/  lista a linha e campo que from e to
+  cnab.rem                                  do cnab na pasta especificada
+
+Faltando argumentos obrigatórios: f, t, s
 ```
-
-hoje a ferramenta busca uma posição e loga isso no terminal.
-
-desafio consiste:
-
-* poder passar na CLI o local do arquivo.
-* pesquisar por nome da empresa, e mostrar em que posição que ela foi achada e qual o tipo de segmento ela pertence.
-
-* **Bonus**, ler o cnab e escrever um novo arquivo em formato JSON, contendo nome e endereço da empresa.
-
-O candidato tem total liberdade de mudar a estrutura atual desse projeto, a ideía é ver a criatividade de resolver esse problema.
--- cnabRows.js
+++ cnabRows.js
@@ -2,72 +2,146 @@
 import path from 'path'
 import { readFile } from 'fs/promises'
 import { fileURLToPath } from 'url';
-
+import fs from "fs";
 import yargs from 'yargs'
 import chalk from 'chalk'
 
-const optionsYargs = yargs(process.argv.slice(2))
-  .usage('Uso: $0 [options]')
-  .option("f", { alias: "from", describe: "posição inicial de pesquisa da linha do Cnab", type: "number", demandOption: true })
-  .option("t", { alias: "to", describe: "posição final de pesquisa da linha do Cnab", type: "number", demandOption: true })
-  .option("s", { alias: "segmento", describe: "tipo de segmento", type: "string", demandOption: true })
-  .example('$0 -f 21 -t 34 -s p', 'lista a linha e campo que from e to do cnab')
-  .argv;
+const COMPANY_START_ROW = 33;
+const COMPANY_END_ROW = 73;
+const ADDRESS_START_ROW = 73;
+const ADDRESS_END_ROW = 153;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const file = path.resolve(`${__dirname}/cnabExample.rem`)
+const optionsYargs = setupYargs();
+const { from, to, segmento, dir = getDefaultDir(), company } = optionsYargs
 
-const { from, to, segmento } = optionsYargs
-
-const sliceArrayPosition = (arr, ...positions) => [...arr].slice(...positions)
-
-const messageLog = (segmento, segmentoType, from, to) => `
------ Cnab linha ${segmentoType} -----
-
-posição from: ${chalk.inverse.bgBlack(from)}
-
-posição to: ${chalk.inverse.bgBlack(to)}
-
-item isolado: ${chalk.inverse.bgBlack(segmento.substring(from - 1, to))}
-
-item dentro da linha P: 
-  ${segmento.substring(0, from)}${chalk.inverse.bgBlack(segmento.substring(from - 1, to))}${segmento.substring(to)}
-
------ FIM ------
-`
-
-const log = console.log
-
-console.time('leitura Async')
-
-readFile(file, 'utf8')
+function main( ){
+  readFile(dir, 'utf8')
   .then(file => {
     const cnabArray = file.split('\n')
-
-    const cnabHeader = sliceArrayPosition(cnabArray, 0, 2)
-
-    const [cnabBodySegmentoP, cnabBodySegmentoQ, cnabBodySegmentoR] = sliceArrayPosition(cnabArray, 2, -2)
-
-    const cnabTail = sliceArrayPosition(cnabArray, -2)
-
-    if (segmento === 'p') {
-      log(messageLog(cnabBodySegmentoP, 'P', from, to))
-      return
+    
+    if (company) {
+      handleCompanySearch(cnabArray, company);
+      return;
     }
-
-    if (segmento === 'q') {
-      log(messageLog(cnabBodySegmentoQ, 'Q', from, to))
-      return
-    }
-
-    if (segmento === 'r') {
-      log(messageLog(cnabBodySegmentoR, 'R', from, to))
-      return
-    }
-
+    
+    handleSegmentSearch(cnabArray, segmento, from, to);
   })
   .catch(error => {
     console.log("🚀 ~ file: cnabRows.js ~ line 76 ~ error", error)
   })
+}
+
+
+function setupYargs() {
+  return yargs(process.argv.slice(2))
+  .usage('Uso: $0 [options]')
+  .option("f", { alias: "from", describe: "posição inicial de pesquisa da linha do Cnab", type: "number", demandOption: true })
+  .option("t", { alias: "to", describe: "posição final de pesquisa da linha do Cnab", type: "number", demandOption: true })
+  .option("s", { alias: "segmento", describe: "tipo de segmento", type: "string", demandOption: true })
+  .option("d", { alias: "dir", describe: "caminho do arquivo", type: "string"})
+  .option("c", { alias: "company", describe: "nome da empresa", type: "string" })
+  .example('$0 -f 21 -t 34 -s p', 'lista a linha e campo que from e to do cnab')
+  .example('$0 -f 21 -t 34 -s p -c BRASIL', 'lista as empresas que contem BRASIL no nome e salva no JSON')
+  .example('$0 -f 21 -t 34 -s p -d ./files/cnab.rem', 'lista a linha e campo que from e to do cnab na pasta especificada')
+  .argv;
+}
+
+function getDefaultDir() {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  return path.resolve(`${__dirname}/cnabExample.rem`);
+}
+
+function sliceArrayPosition(arr, ...positions) {
+  return [...arr].slice(...positions);
+}
+
+function messageLog(segmento, segmentoType, from, to) {
+  return `
+  ----- Cnab linha ${segmentoType} -----
+  
+  posição from: ${chalk.inverse.bgBlack(from)}
+  
+  posição to: ${chalk.inverse.bgBlack(to)}
+  
+  item isolado: ${chalk.inverse.bgBlack(segmento.substring(from - 1, to))}
+  
+  item dentro da linha ${segmentoType}: 
+  ${segmento.substring(0, from)}${chalk.inverse.bgBlack(segmento.substring(from - 1, to))}${segmento.substring(to)}
+  
+  ----- FIM ------
+  `;
+}
+
+function handleCompanySearch(cnabArray, company) {
+  const array = sliceArrayPosition(cnabArray, 2, -2);
+  const lineDataList = getLineDataList(array, company);
+  const jsonList = getJsonList(lineDataList);
+  writeToFile(jsonList);
+}
+
+function getLineDataList(array, company) {
+  return array.map((row) => {
+    if(row.includes(company)){
+      const initial = row.search(company)+1
+      if(initial>= COMPANY_START_ROW && initial < COMPANY_END_ROW){
+        return {
+          row,
+          initial,
+          final: initial+company.length-1,
+        }
+      }
+    }
+  }).filter(Boolean);
+}
+
+function getJsonList(lineDataList) {
+  const jsonList = [];
+  if (lineDataList.length) {
+    lineDataList.forEach((lineData)=>{
+      const segmentType = lineData.row.split('').find(
+        letter => ['P', 'Q', 'R'].includes(letter)
+        )
+        console.log(messageLog(lineData.row, segmentType, lineData.initial, lineData.final))
+        jsonList.push({
+          companyName: lineData.row.substring(COMPANY_START_ROW, COMPANY_END_ROW).trim(),
+          companyAddress: lineData.row.substring(ADDRESS_START_ROW, ADDRESS_END_ROW).trim(),
+        })
+      })
+    }
+    return jsonList;
+  }
+  
+  function writeToFile(jsonList) {
+    fs.writeFile("cnabCompanies.json", JSON.stringify(jsonList), (err) => {
+      if (err) {
+        console.error("Error on write File:", err);
+      } else {
+        console.log("File created successfully");
+      }
+    });
+  }
+  
+  function handleSegmentSearch(cnabArray, segmento, from, to) {
+    const [cnabBodySegmentoP, cnabBodySegmentoQ, cnabBodySegmentoR] = sliceArrayPosition(cnabArray, 2, -2);
+    sliceArrayPosition(cnabArray, -2);
+    
+    if (segmento === 'p') {
+      console.log(messageLog(cnabBodySegmentoP, 'P', from, to));
+      return;
+    }
+    
+    if (segmento === 'q') {
+      console.log(messageLog(cnabBodySegmentoQ, 'Q', from, to));
+      return;
+    }
+    
+    if (segmento === 'r') {
+      console.log(messageLog(cnabBodySegmentoR, 'R', from, to));
+      return;
+    }
+  }
+  
+console.time('leitura Async')
+main()
 console.timeEnd('leitura Async')
